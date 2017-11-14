90dc526edab473287b0421afbbf1221d072adbc7
Mihai Peteu <jericho1ne@yahoo.com>
2017-11-14 10:10:34 -0800

    Getting FSC Mobile off the ground!

---
+++ js/common.js
@@ -0,0 +1,67 @@
+/**
+ * Provides a clean way to print data to console
+ * @param  {[type]} items [description]
+ * @return {[type]}       [description]
+ */
+function logArray(items) {
+	items.forEach((item) => {
+		for (var propName in item) {
+			console.log(propName + ' : ' + item[propName]);
+		}
+		console.log(' ------------------------------------------------ ');
+	});
+}
+
+/**
+ * Ajax call to data source
+ * @param {string} endpoint (eg: search, cities, etc)
+ * @param {string} requestUrl GET url params to be appended
+ * @return {Promise} 
+ */
+function fetchDataFromApi(endpoint, urlParams) {
+	var requestUrl = 
+		'https://api.findsomecoffee.com/' 
+		+ endpoint + '?' + urlParams;
+	
+	console.log(requestUrl);
+	
+	return fetch(requestUrl)
+		.then(function(response) { 
+			return response.json();
+		})
+		.then(function(response) {
+			if (typeof response.businesses === 'object') {
+				var items = response.businesses;
+				
+				// Sort based on proximity
+				items.sort(function(a, b) {		
+					// sort by proximity (closest first)
+					return parseFloat(a.distance) - parseFloat(b.distance);
+				});
+
+				// Convert meters to miles, customize display value
+				items.forEach((item) => {
+					logArray(item.name);
+
+					// Add a custom distance parameter
+					var thisFarAway = (item.distance * 0.000621371).toFixed(1);
+					item.distance = thisFarAway < 0.1 
+					 	? `* Really Close *`
+					 	: `${thisFarAway} miles away`;
+
+					// Replace the larger original image with a smaller one
+					const pattern = /o.jpg/;
+					const smaller_img = item.image_url.replace( pattern, "l.jpg" );
+					item.image_url = smaller_img;
+				});
+				return items;
+			} else {
+				return {};
+			}
+	});
+} // End fetchDataFromApi
+
+module.exports = {
+	fetchDataFromApi: fetchDataFromApi,
+	logArray: logArray,
+};
---
+++ js/data.js
@@ -0,0 +1,38 @@
+exports.items = [
+	{
+		"id": "0",
+		"artist": "Com Truise",
+		"venue": "Echoplex",
+		"description": "Null is one of the most mysterious figures in the ambientwave genre to date. His twenty hour long drone mixes will make you float away into another dimension.",
+		"image_url": "https://s3-media3.fl.yelpcdn.com/bphoto/1IxGKmPpBKlEe_eIUdHMMA/o.jpg",
+		"date": "February 5",
+		"price": "29.99"
+	},
+	{
+		"id": "1",
+		"artist": "Shins",
+		"venue": "Hi Hat",
+		"description": "Since they formed in 2010, the foobars have been busy rocking the nation with their signature blend of post-dubstep and dark metal.",
+		"image_url": "Assets/EventImages/image0.jpg",
+		"date": "January 20",
+		"price": "23.99"
+	},
+	{
+		"id": "2",
+		"artist": "VRTSV",
+		"venue": "Echoplex",
+		"description": "Easily one of the most anticipated acts of the digital millenia. If you don't already own their complete discography on gold-plated vinyl, you're simply not cool enough. Plus more text, waay more text, waay more text, waay more text, waay more text, waay more text, waay more text, waay more text, ",
+		"image_url": "Assets/EventImages/image2.jpg",
+		"date": "February 16",
+		"price": "49.99"
+	},
+	{
+		"id": "3",
+		"artist": "Fourth Thing",
+		"venue": "Regent DTLA",
+		"description": "Blah.",
+		"image_url": "Assets/EventImages/image2.jpg",
+		"date": "February 20",
+		"price": "Free"
+	}
+];
